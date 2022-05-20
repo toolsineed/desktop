37b098da6d2e89849d0b0eb8da7c1b20d114974c
PadowYT2 <me@padow.ru>
2022-05-20 22:10:51 +0800

    Add support for detecting PhpStorm editor (#717)

--- app/src/lib/editors/linux.ts
+++ app/src/lib/editors/linux.ts
@@ -112,6 +112,10 @@ const editors: ILinuxExternalEditor[] = [
     name: 'Mousepad',
     paths: ['/usr/bin/mousepad'],
   },
+  {
+    name: 'IntelliJ PhpStorm',
+    paths: ['/snap/bin/phpstorm'],
+  },
 ]
 
 async function getAvailablePath(paths: string[]): Promise<string | null> {
