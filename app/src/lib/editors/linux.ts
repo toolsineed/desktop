68e0f913e6ecca4d7eafbe6b4d5f12af1fadb3c0
Brendan Forster <github@brendanforster.com>
2023-08-06 11:24:07 -0300

    backport patches from previous release (#915)
    * remove rpm section as unnecessary
    
    * strip out checksum generation for release
    
    * include .sha256 artifacts in release
    
    * update electron to latest minor release
    
    * Apply suggestions from code review

--- app/src/lib/editors/linux.ts
+++ app/src/lib/editors/linux.ts
@@ -152,10 +152,6 @@ const editors: ILinuxExternalEditor[] = [
     name: 'Mousepad',
     paths: ['/usr/bin/mousepad'],
   },
-  {
-    name: 'IntelliJ PhpStorm',
-    paths: ['/snap/bin/phpstorm'],
-  },
 ]
 
 async function getAvailablePath(paths: string[]): Promise<string | null> {
